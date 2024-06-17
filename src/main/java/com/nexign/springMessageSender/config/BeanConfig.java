82e55dd4a7438c7bfc6a838b777ba651dd83d405
Mityasov Aleksandr <aleksandr.mityasov@nexign.com>
2024-06-17 13:06:12 +0300

    Spring 17.06_4

--- src/main/java/com/nexign/springMessageSender/config/BeanConfig.java
+++ src/main/java/com/nexign/springMessageSender/config/BeanConfig.java
@@ -4,6 +4,9 @@ import com.nexign.springMessageSender.model.Message;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("com.nexign.springMessageSender")
@@ -12,5 +15,16 @@ public class BeanConfig {
     @Bean
     public Message getMessage() {
         return new Message();
+    }
+
+    public DataSource dataSource() {
+        return new DriverManagerDataSource(
+                "jdbc:h2:mem:testdb;" +
+                        "DB_CLOSE_DELAY=-1;" +
+                        "DB_CLOSE_ON_EXIT=false;" +
+                        "DATABASE_TO_UPPER=false;",
+                "sa",
+                ""
+        );
     }
 }
--- src/main/java/com/nexign/springMessageSender/service/MessageSender.java
+++ src/main/java/com/nexign/springMessageSender/service/MessageSender.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MessageSender {
-    @Autowired
+
     private IMessage message;
     private Destination destination;
 
